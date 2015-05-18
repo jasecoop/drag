26b43ae34f2cdabaece4519c2517c4d9516188b8
jasecoop <jase@jasecoop.com>
2015-05-18 01:13:06 +0100

    Carrier wave working

--- app/controllers/images_controller.rb
+++ app/controllers/images_controller.rb
@@ -77,6 +77,6 @@ class ImagesController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def image_params
-      params.require(:image).permit(:url, :source, :title, :desc)
+      params.require(:image).permit(:url, :source, :title, :desc, :file)
     end
 end
