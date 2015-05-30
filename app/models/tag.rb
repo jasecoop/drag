07a912cf5b98681da98b89d7ab53e12426b14dd4
jasecoop <jase@jasecoop.com>
2015-05-30 11:03:02 +0100

    removed files

--- app/models/tag.rb
+++
@@ -1,9 +0,0 @@
-class Tag < ActiveRecord::Base
-  belongs_to :user
-  has_many :taggings
-  has_many :posts, through: :taggings
-
-  def self.counts
-    self.select("name, count(taggings.tag_id) as count").joins(:taggings).group("taggings.tag_id")
-  end
-end
--- db/migrate/20150526194748_add_image_id_to_taggings.rb
+++
@@ -1,5 +0,0 @@
-class AddImageIdToTaggings < ActiveRecord::Migration
-  def change
-    add_column :taggings, :image_id, :integer
-  end
-end
