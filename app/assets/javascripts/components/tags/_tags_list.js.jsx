f555d644bb84e241a1342f2fc6448fbc04087dc7
jasecoop <jase@jasecoop.com>
2015-06-03 22:27:58 +0100

    Animate Upload items

--- app/assets/javascripts/components/tags/_tags_list.js.jsx
+++ app/assets/javascripts/components/tags/_tags_list.js.jsx
@@ -1,11 +1,13 @@
 var TagsList = React.createClass({
   render: function () {
     return (
-      <ul className="tags-list">
-        {this.props.tags.map(function (tag) {
-            return <Tag tag={tag}/>
-        })}
-      </ul>
+      <div className="tags-list">
+        <ul>
+          {this.props.tags.map(function (tag) {
+              return <Tag tag={tag}/>
+          })}
+        </ul>
+      </div>
     );
   }
 });
