542ecc8e9e602c7aa603881ff6162c7659b1cd63
jasecoop <jase@jasecoop.com>
2015-07-28 14:23:34 +0100

    home

--- app/assets/javascripts/components/site/_home.js.jsx
+++ app/assets/javascripts/components/site/_home.js.jsx
@@ -6,7 +6,30 @@ var Home = React.createClass({
   render: function() {
     return (
       <div className="page-home">
-        <Signup />
+        <div className="box">
+          <div className="box__body">
+            <h1>Drag</h1>
+            <h2>Save what you see</h2>
+
+            <p>Image collections for professionals.</p>
+
+            <ul>
+
+              <li>$20 per year.</li>
+
+              <li>Public and private collections of images.</li>
+
+              <li>Hi-res. View images at the resolution they deserve.</li>
+
+              <li>Customiable layout to suit your images context.</li>
+
+              <li>No cupcakes, motiviational quotes and Pinteresters.</li>
+
+              <li><a href="">Learn more about why we built Drag</a></li>
+
+            </ul>
+          </div>
+        </div>
       </div>
     )
   }
