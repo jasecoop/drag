ad174da4279cfba4d79e6abfca1caa0e2d0db88e
jasecoop <jase@jasecoop.com>
2015-08-13 14:06:41 +0100

    heroku fix

--- config/initializers/fog.rb
+++ config/initializers/fog.rb
@@ -1,13 +1,5 @@
 ::FOG = Fog::Storage.new({
   :provider              =>'AWS',
-  :aws_access_key_id     =>ENV['AWS_ACCESS_KEY_ID'],
-  :aws_secret_access_key =>ENV['AWS_SECRET_ACCESS_KEY']
+  :aws_access_key_id     =>'AKIAIBZMVRXXRDNLVE7A',
+  :aws_secret_access_key =>'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'
 })
-
-CarrierWave.configure do |config|
-  config.fog_credentials = {
-  :provider              =>'AWS',
-  :aws_access_key_id     =>ENV['AWS_ACCESS_KEY_ID'],
-  :aws_secret_access_key =>ENV['AWS_SECRET_ACCESS_KEY']
-  }
-end
