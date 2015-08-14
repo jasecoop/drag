1b56f793dceb010334cd5afa7e71dbc50458d593
jasecoop <jase@jasecoop.com>
2015-08-14 18:26:30 +0100

    home

--- app/assets/javascripts/components/site/_home.js.jsx
+++ app/assets/javascripts/components/site/_home.js.jsx
@@ -3,32 +3,51 @@ var Signup = require('../sessions/_reserve_username');
 
 var Home = React.createClass({
 
+  _onSignUpClick: function (e) {
+    var username = this.refs.signupUsername.getDOMNode().value
+    var email = this.refs.signupEmail.getDOMNode().value
+    var password = this.refs.signupPassword.getDOMNode().value
+
+    var user = new Parse.User();
+    user.set("username", username);
+    user.set("password", password);
+    user.set("email", email);
+
+    var self = this;
+
+    user.signUp(null, {
+      success: function(user) {
+        window.location.replace("/");
+        delete self;
+      },
+      error: function(user, error) {
+        alert("Error: " + error.code + " " + error.message);
+      }
+    });
+
+    e.preventDefault();
+  },
+
   render: function() {
     return (
       <div className="page-home">
-        <div className="box">
-          <div className="box__body">
-            <h1>Drag</h1>
-            <h2>Save what you see</h2>
-
-            <p>Image collections for professionals.</p>
-
-            <ul>
-
-              <li>$20 per year.</li>
-
-              <li>Public and private collections of images.</li>
-
-              <li>Hi-res. View images at the resolution they deserve.</li>
-
-              <li>Customiable layout to suit your images context.</li>
-
-              <li>No cupcakes, motiviational quotes and Pinteresters.</li>
-
-              <li><a href="">Learn more about why we built Drag</a></li>
-
-            </ul>
-          </div>
+        <div className="home-box form-fix">
+          <form>
+            <div className="form-fix__body">
+                <div className="input input-full mb2">
+                  <input onChange= {this._handleInputChange} ref="signupUsername" type="text" placeholder="Username"></input>
+                </div>
+                <div className="input input-full mb2">
+                  <input onChange= {this._handleInputChange} ref="signupEmail" type="email" placeholder="Email address"></input>
+                </div>
+                <div className="input input-full">
+                  <input onChange= {this._handleInputChange} ref="signupPassword" type="password" placeholder="Password"></input>
+                </div>
+            </div>
+            <div className="form-fix__footer">
+              <button className="btn btn-black" onClick={this._onSignUpClick}>Reserve your username</button>
+            </div>
+          </form>
         </div>
       </div>
     )
